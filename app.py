d3fd17bc6070e7f5c7b79796d4ada128ca7dc179
kumar-shivang <shivangkumar1857@gmail.com>
2024-12-31 18:57:32 +0530

    added .gitignore

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import os
-from typing import Annotated
 
 import streamlit as st
 from dotenv import load_dotenv
 
-# from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from langchain_core.prompts import PromptTemplate
 from langchain_mistralai import ChatMistralAI
 from langgraph.graph import END, START, StateGraph
 from langgraph.graph.message import add_messages
-from typing_extensions import TypedDict
+from graph import graph_builder
 
 load_dotenv()
 
@@ -18,5 +16,14 @@ model = ChatMistralAI(
     model_name="mistral-medium", api_key=os.getenv("MISTRALAI_API_KEY")
 )
 
+graph_builder.add_node("model", model)  # adding the model to the graph
+graph_builder.add_edge(START, "model")
+
+graph = graph_builder.compile()
+
+with open("graph.txt", "w") as file:
+    file.write(graph.get_graph().draw_ascii())
+
+
 st.title("Langchain")
-st.write(model.invoke("Hi").content)
+st.write("placeholder text")
