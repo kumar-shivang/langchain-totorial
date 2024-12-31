3da673170d27c1a70fe80d239357bb716e40cad9
kumar-shivang <shivangkumar1857@gmail.com>
2025-01-01 01:29:54 +0530

    updated app.py to work with search tool

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
+from tools import tool_node, route_tools
+from graph import State, graph_builder
+from langgraph.graph import END, START
+from langchain_mistralai import ChatMistralAI
+from tools import search
 import os
 
-import streamlit as st
 from dotenv import load_dotenv
-
-from langchain_core.prompts import PromptTemplate
-from langchain_mistralai import ChatMistralAI
-from langgraph.graph import END, START, StateGraph
-from langgraph.graph.message import add_messages
-from graph import graph_builder
 
 load_dotenv()
 
-
-model = ChatMistralAI(
-    model_name="mistral-medium", api_key=os.getenv("MISTRALAI_API_KEY")
+llm = ChatMistralAI(
+    model_name="mistral-large-2407", api_key=os.getenv("MISTRALAI_API_KEY")
 )
 
-graph_builder.add_node("model", model)  # adding the model to the graph
-graph_builder.add_edge(START, "model")
+llm = llm.bind_tools(search)
 
+
+def model(state: State):  # for testing
+    return {"messages": [llm.invoke(state["messages"])]}
+
+
+graph_builder.add_node("model", model)  # adding the model to the graph
+graph_builder.add_node("tools", tool_node)
+graph_builder.add_edge(START, "model")
+graph_builder.add_conditional_edges(
+    "model",
+    route_tools,
+    {
+        "tools": "tools",  # NOTE: if route_tools return tools...go to tool_node
+        END: END,  # NOTE: if route_tools return END, go to the END node
+    },
+)
+graph_builder.add_edge("tools", "model")
+# graph_builder.set_entry_point("model")
+# graph_builder.set_finish_point("model")
 graph = graph_builder.compile()
 
 with open("graph.txt", "w") as file:
     file.write(graph.get_graph().draw_ascii())
-
-
-st.title("Langchain")
-st.write("placeholder text")
