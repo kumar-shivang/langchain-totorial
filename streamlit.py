4930a161142f70ff695dfc59a1680336aacd77ef
kumar-shivang <shivangkumar1857@gmail.com>
2025-01-01 01:28:50 +0530

    minor changes

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,3 @@
-from langchain_core.messages import HumanMessage
-
 from app import graph
 import streamlit as st
 
@@ -10,9 +8,9 @@ input_text = st.text_input("write something")
 stream = st.checkbox("Stream text")
 if input_text:
     if stream:
-        # BUG:- It's not working as expected, would fix it later
+        # FIX:- It's not working as expected, would fix it later
         st.write_stream(graph.stream({"messages": [("user", input_text)]}))
     else:
-        st.write(
-            graph.invoke({"messages": [("user", input_text)]})["messages"][-1].content
-        )
+        output = graph.invoke({"messages": [("user", input_text)]})["messages"][-1]
+        st.write(output.content)
+        # FIX: find a way to check output tool_calls
